9840be73e60164d23bdf927f10681ec93c791586
Nikolay <nikolay@ginkgoukraine.com>
2020-06-12 15:49:25 +0300

    add 'context_processors.py' to app 'supplieraccount' for have access to supplier_accounts in every template

---
+++ supplieraccount/context_processors.py
@@ -0,0 +1,18 @@
+from .models import SupplierAccount, SupplierCodes
+
+
+def selected_supplier_accounts(request):
+    if not request.user.is_anonymous:
+        accounts = SupplierAccount.objects.filter(usersupplieraccount__user=request.user)
+        if len(accounts) == 0:
+            return {'selected_supplier_accounts': None,
+                    'suppliers': SupplierCodes.SUPPLIERS}
+        else:
+
+            return {'selected_supplier_accounts': {supplier_code: SupplierAccount.objects.filter(supplier=supplier_code,
+                                                   usersupplieraccount__user=request.user)
+                                                   for supplier_code in SupplierCodes.SUPPLIERS},
+                    'suppliers': SupplierCodes.SUPPLIERS}
+    else:
+        return {'selected_supplier_accounts': None,
+                'suppliers': SupplierCodes.SUPPLIERS}
