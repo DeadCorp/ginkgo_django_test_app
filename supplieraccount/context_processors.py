d4d3c9c3538331c91c7bc6fefbd6a780297163cb
Nikolay <nikolay@ginkgoukraine.com>
2020-06-19 15:09:31 +0300

    test

--- supplieraccount/context_processors.py
+++ supplieraccount/context_processors.py
@@ -6,13 +6,16 @@ def selected_supplier_accounts(request):
         accounts = SupplierAccount.objects.filter(usersupplieraccount__user=request.user)
         if len(accounts) == 0:
             return {'selected_supplier_accounts': None,
-                    'suppliers': SupplierCodes.SUPPLIERS}
+                    'suppliers': SupplierCodes.SUPPLIERS,
+                    'all_supplier_accounts': SupplierAccount.objects.all()}
         else:
 
             return {'selected_supplier_accounts': {supplier_code: SupplierAccount.objects.filter(supplier=supplier_code,
-                                                   usersupplieraccount__user=request.user)
+                                                                                                 usersupplieraccount__user=request.user)
                                                    for supplier_code in SupplierCodes.SUPPLIERS},
-                    'suppliers': SupplierCodes.SUPPLIERS}
+                    'suppliers': SupplierCodes.SUPPLIERS,
+                    'all_supplier_accounts': SupplierAccount.objects.all()}
     else:
         return {'selected_supplier_accounts': None,
-                'suppliers': SupplierCodes.SUPPLIERS}
+                'suppliers': SupplierCodes.SUPPLIERS,
+                'all_supplier_accounts': SupplierAccount.objects.all()}
